4efd680e6be77a59e46f0b1295cf440106d79c01
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2025-06-13 05:45:16 -0700

    add hero to structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main className="main-content">
+        <Hero />
         <About />
         <Projects />
         <Skills />
