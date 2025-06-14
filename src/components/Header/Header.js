aa8f12efaf929c4772a103042ebffa3a07a1d5b7
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2025-06-13 22:23:54 -0700

    structure nav

---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./Header.css";
+
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  return (
+    <header className="header">
+      <div className="logo">
+        <span className="logo-text">
+          <a href="#header">Matias Agado</a>
+        </span>
+        <div className="logo-dot"></div>
+      </div>
+
+      <nav className={`nav ${isMenuOpen ? "nav-open" : ""}`}>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a href="#about">About Me</a>
+          </li>
+          <li className="nav-item">
+            <a href="#projects">Pet Projects</a>
+          </li>
+          <li className="nav-item">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      <button className="menu-toggle" onClick={toggleMenu}>
+        <span className="menu-icon"></span>
+      </button>
+    </header>
+  );
+}
+
+export default Header;
