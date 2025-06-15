08014f4dbdd1b4058b88417d963cf2d37eb578b6
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2025-06-14 18:41:52 -0700

    App.js: add Hero Section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 
 import Header from "./components/Header/Header";
+import Hero from "./components/Header/Hero";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main className="main-content">
+        <Hero />
+      </main>
     </div>
   );
 }
