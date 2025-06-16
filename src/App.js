7706be447e5450e4a9c4e3925f5d1204331620c6
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2025-06-15 20:13:25 -0700

    App.js: add about section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <main className="main-content">
         <Hero />
+        <About />
       </main>
     </div>
   );
