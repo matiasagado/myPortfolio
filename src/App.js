c52c457e46f89779178c449d8a3eafda5d56efe3
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2025-06-16 01:05:32 -0700

    add Footer Section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Projects />
         <Contact />
       </main>
+      <Footer />
     </div>
   );
 }
