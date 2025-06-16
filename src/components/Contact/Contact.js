dd8643b0f06c785b924b14fd236953072094f9bf
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2025-06-16 01:04:28 -0700

    init commit

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <section className="section" id="contact">
+      <div className="section-header">
+        <span className="section-title">Contact Me</span>
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
