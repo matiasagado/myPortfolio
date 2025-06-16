bb371b93fd0c2b3521eeced3f34f2936fd617ba1
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2025-06-16 01:03:52 -0700

    add Contact Section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Hero />
         <About />
         <Projects />
+        <Contact />
       </main>
     </div>
   );
