77b533dcdb81ce06e3ffb02fb593ee7264d5af7b
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2025-06-15 21:30:38 -0700

    add JSX

---
+++ src/components/About/About.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  const one = (
+    <p>
+      I am currently a <b>Senior</b> at the{" "}
+      <a href="https://www.usfca.edu/">University of San Francisco</a>, pursuing
+      a<b> Bachelor's of Science</b> in <b>Computer Science</b>. I'm passionate
+      about software development and always eager to explore new technologies
+      and tackle real-world problems.
+    </p>
+  );
+
+  const two = (
+    <p>
+      Outside of coursework, I'm interested in following the developments of
+      science. I also like to play Lacrosse.
+    </p>
+  );
+
+  const tech_stack = [
+    "React.js",
+    "JavaScript",
+    "Java",
+    "Python",
+    "MongoDB",
+    "HTML/CSS",
+  ];
+
+  return (
+    <div className="about">
+      <div className="section-header">
+        <span className="section-title">/ about me</span>
+      </div>
+      <div className="about-content">
+        <div className="about-description">
+          {one}
+          <p>Here are some technologies I've been working with:</p>
+          <ul className="tech-stack">
+            {tech_stack.map((tech_item, i) => (
+              <li key={i}>{tech_item}</li>
+            ))}
+          </ul>
+          {two}
+        </div>
+        <div className="about-image">
+          <img alt="Matias Agado" src="/assets/me2.jpg" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
